2c97c08248679d5fb84d3af188bdcaf3a58b49bb
Tokaallah <tokallah.92@gmail.com>
2023-09-30 03:04:11 +0000

    [Fix] erorrs

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
-"""
-A python script that starts a Flask web application
-"""
-from flask import Flask, Blueprint, abort, jsonify
+'''
+    app for registering blueprint and starting flask
+'''
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """
-    Function that shows a 404 error
-    """
-    return (jsonify(error="Not found"), 404)
 
 
 @app.teardown_appcontext
-def teardown(exception=None):
-    """
-     Function closes the current session
-    """
+def tear_down(self):
+    '''
+    close query after each session
+    '''
     storage.close()
 
 
-if __name__ == '__main__':
-    h = getenv('HBNB_API_HOST')
-    p = getenv('HBNB_API_PORT')
-    app.run(host=h, port=p, threaded=True)
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == "__main__":
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-"""Creationg route for Blueprint
-"""
+'''
+    flask with general routes
+    routes:
+        /status:    display "status":"OK"
+        /stats:     dispaly total for all classes
+'''
 from api.v1.views import app_views
-from models import storage
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status')
-def response():
-    """ get status ok
-    """
-    dic = {"status": "OK"}
-    return jsonify(dic)
+@app_views.route("/status")
+def status():
+    '''
+        return JSON of OK status
+    '''
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats')
-def class_counter():
-    """ get a dictionary from count method
-    """
-    dic = {}
-    dic["amenities"] = storage.count("Amenity")
-    dic["cities"] = storage.count("City")
-    dic["places"] = storage.count("Place")
-    dic["reviews"] = storage.count("Review")
-    dic["states"] = storage.count("State")
-    dic["users"] = storage.count("User")
-    return jsonify(dic)
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(cls_counts)
